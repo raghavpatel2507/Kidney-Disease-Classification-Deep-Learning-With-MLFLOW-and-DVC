4a71cc1625149d3f30c1bed513c469c7f1cb5905
raghav patel <patelraghav014@gmail.com>
2024-03-26 18:06:58 +0530

    update

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def predictRoute():
 if __name__ == "__main__":
     clApp = ClientApp()
     #hello i am raghav patel
-    app.run(host='0.0.0.0', port=5000) 
+    app.run(host='0.0.0.0', port=8080) 
 
